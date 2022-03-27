51b82ae86aa0fdb1b3da4f6fa3fa54a4c14fa4cc
Afham <82728352+afham-b@users.noreply.github.com>
2022-03-27 18:02:53 +0000

    void get_ymd function was fixed

--- main.cpp
+++ main.cpp
@@ -5,17 +5,17 @@ void get_ymd ( int date, int& year, int& month, int& day);
 
 int main() {
   int date; 
-  int y, m ,d; 
+  int year, month ,day; 
   cout << "Ener a 8 digit date: " <<endl; 
   cin >> date; 
-  get_ymd (date, y, m, d); 
-  cout << m << "/" << d << "/" << y << endl; 
+  get_ymd (date, year, month, day); 
+  cout << month << "/" << day << "/" << year << endl; 
   return 0;  
 }
 
 void get_ymd (int date, int& year, int& month, int& day) { 
-  day= date % 100; 
-  month = date /100 % 100; 
-  year = date / 10000; 
+  day= date % 1000000 / 10000; 
+  month = date /1000000; 
+  year = date % 10000; 
 
 }
